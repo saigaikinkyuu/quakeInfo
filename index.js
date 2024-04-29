aaf2b8834f0c106c26ffcbdcf4f238a9c8da7566
saigaikinkyuu <125773463+saigaikinkyuu@users.noreply.github.com>
2024-04-29 15:54:43 +0900

    Update index.js

--- index.js
+++ index.js
@@ -14,81 +14,113 @@ $.getJSON("./prefectures.geojson", function (data) {
     L.geoJson(data, {
         style: PolygonLayer_Style_nerv
     }).addTo(map);
-});
 
-$.getJSON("https://api.p2pquake.net/v2/history?codes=551", function (data) {
-    let maxInt_data = data[0]['earthquake']['maxScale'];
-    var maxIntText = maxInt_data == 10 ? "1" : maxInt_data == 20 ? "2" : maxInt_data == 30 ? "3" : maxInt_data == 40 ? "4" :
-                     maxInt_data == 45 ? "5弱" : maxInt_data == 46 ? "5弱" : maxInt_data == 50 ? "5強" : maxInt_data == 55 ? "6弱" :
-                     maxInt_data == 60 ? "6強" : maxInt_data == 70 ? "7" : "不明";
-    
-    var Magnitude = data[0]['earthquake']['hypocenter']['magnitude'] != -1 ?
-                    (data[0]['earthquake']['hypocenter']['magnitude']).toFixed(1) : 'ー.ー';
-    var Name = data[0]['earthquake']['hypocenter']['name'] != "" ?
-               data[0]['earthquake']['hypocenter']['name'] : '情報なし';
-    var Depth = data[0]['earthquake']['hypocenter']['depth'] != -1 ?
-                "約"+data[0]['earthquake']['hypocenter']['depth']+"km" : '不明';
-    var tsunamiText = data[0]['earthquake']['domesticTsunami'] == "None" ? "なし" :
-                      data[0]['earthquake']['domesticTsunami'] == "Unknown" ? "不明" :
-                      data[0]['earthquake']['domesticTsunami'] == "Checking" ? "調査中" :
-                      data[0]['earthquake']['domesticTsunami'] == "NonEffective" ? "若干の海面変動" :
-                      data[0]['earthquake']['domesticTsunami'] == "Watch" ? "津波注意報" :
-                      data[0]['earthquake']['domesticTsunami'] == "Warning" ? "津波警報" : "情報なし";
-    var Time = data[0]['earthquake']['time'];
-    
-    var shingenLatLng = new L.LatLng(data[0]["earthquake"]["hypocenter"]["latitude"], data[0]["earthquake"]["hypocenter"]["longitude"]);
-    var shingenIconImage = L.icon({
-        iconUrl: 'source/shingen.png',
-        iconSize: [40, 40],
-        iconAnchor: [20, 20],
-        popupAnchor: [0, -40]
-    });
-    var shingenIcon = L.marker(shingenLatLng, {icon: shingenIconImage }).addTo(map);
-    shingenIcon.bindPopup('発生時刻：'+Time+'<br>最大震度：'+maxIntText+'<br>震源地：'+Name+'<span style=\"font-size: 85%;\"> ('+data[0]["earthquake"]["hypocenter"]["latitude"]+", "+data[0]["earthquake"]["hypocenter"]["longitude"]+')</span><br>規模：M'+Magnitude+'　深さ：'+Depth+'<br>受信：'+data[0]['issue']['time']+', '+data[0]['issue']['source'],{closeButton: false, zIndexOffset: 10000, maxWidth: 10000});
-    shingenIcon.on('mouseover', function (e) {this.openPopup();});
-    shingenIcon.on('mouseout', function (e) {this.closePopup();});
+    $.getJSON("https://api.p2pquake.net/v2/history?codes=551", function (quakeData) {
+        var maxIntensity = quakeData[0]['earthquake']['maxScale'];
 
-    // Add coloring based on seismic intensity to prefectures.geojson
-    $.each(data[0]['points'], function(index, point) {
-        var prefectureName = point['pref'];
-        var seismicIntensity = point['scale'];
-
-        // Define colors based on seismic intensity
-        var color = '';
-        switch (seismicIntensity) {
+        var maxIntensityText = '';
+        switch (maxIntensity) {
             case 10:
-                color = 'red';
+                maxIntensityText = "1";
                 break;
             case 20:
-                color = 'orange';
+                maxIntensityText = "2";
                 break;
             case 30:
-                color = 'yellow';
+                maxIntensityText = "3";
                 break;
-            // Add more cases as needed for other seismic intensities
+            case 40:
+                maxIntensityText = "4";
+                break;
+            case 45:
+            case 46:
+                maxIntensityText = "5弱";
+                break;
+            case 50:
+                maxIntensityText = "5強";
+                break;
+            case 55:
+                maxIntensityText = "6弱";
+                break;
+            case 60:
+                maxIntensityText = "6強";
+                break;
+            case 70:
+                maxIntensityText = "7";
+                break;
             default:
-                color = 'gray';
+                maxIntensityText = "不明";
         }
 
-        // Find the corresponding prefecture in the geojson data and update its style
-        $.each(data.features, function(index, feature) {
-            if (feature.properties.pref === prefectureName) {
-                feature.properties['fillColor'] = color;
-                feature.properties['fillOpacity'] = 0.5; // Adjust opacity as needed
-            }
+        var magnitude = quakeData[0]['earthquake']['hypocenter']['magnitude'] != -1 ?
+                        quakeData[0]['earthquake']['hypocenter']['magnitude'].toFixed(1) : 'ー.ー';
+        var name = quakeData[0]['earthquake']['hypocenter']['name'] != "" ?
+                   quakeData[0]['earthquake']['hypocenter']['name'] : '情報なし';
+        var depth = quakeData[0]['earthquake']['hypocenter']['depth'] != -1 ?
+                    "約" + quakeData[0]['earthquake']['hypocenter']['depth'] + "km" : '不明';
+        var tsunamiText = quakeData[0]['earthquake']['domesticTsunami'] == "None" ? "なし" :
+                          quakeData[0]['earthquake']['domesticTsunami'] == "Unknown" ? "不明" :
+                          quakeData[0]['earthquake']['domesticTsunami'] == "Checking" ? "調査中" :
+                          quakeData[0]['earthquake']['domesticTsunami'] == "NonEffective" ? "若干の海面変動" :
+                          quakeData[0]['earthquake']['domesticTsunami'] == "Watch" ? "津波注意報" :
+                          quakeData[0]['earthquake']['domesticTsunami'] == "Warning" ? "津波警報" : "情報なし";
+        var time = quakeData[0]['earthquake']['time'];
+
+        var shingenLatLng = new L.LatLng(quakeData[0]["earthquake"]["hypocenter"]["latitude"], quakeData[0]["earthquake"]["hypocenter"]["longitude"]);
+        var shingenIconImage = L.icon({
+            iconUrl: 'source/shingen.png',
+            iconSize: [40, 40],
+            iconAnchor: [20, 20],
+            popupAnchor: [0, -40]
         });
-    });
+        var shingenIcon = L.marker(shingenLatLng, {icon: shingenIconImage }).addTo(map);
+        shingenIcon.bindPopup('発生時刻：'+time+'<br>最大震度：'+maxIntensityText+'<br>震源地：'+name+'<span style=\"font-size: 85%;\"> ('+quakeData[0]["earthquake"]["hypocenter"]["latitude"]+", "+quakeData[0]["earthquake"]["hypocenter"]["longitude"]+')</span><br>規模：M'+magnitude+'　深さ：'+depth+'<br>受信：'+quakeData[0]['issue']['time']+', '+quakeData[0]['issue']['source'],{closeButton: false, zIndexOffset: 10000, maxWidth: 10000});
+        shingenIcon.on('mouseover', function (e) {this.openPopup();});
+        shingenIcon.on('mouseout', function (e) {this.closePopup();});
 
-    // Update the map with the modified geojson data
-    L.geoJson(data, {
-        style: function(feature) {
-            return {
-                fillColor: feature.properties.fillColor,
-                fillOpacity: feature.properties.fillOpacity,
-                color: '#ffffff',
-                weight: 1.5,
-                opacity: 1
-            };
-        }
-    }).addTo(map);
+        // Add coloring based on seismic intensity to prefectures.geojson
+        $.each(quakeData[0]['points'], function(index, point) {
+            var prefectureCode = point['pref'];
+            var seismicIntensity = point['scale'];
+
+            // Define colors based on seismic intensity
+            var color = '';
+            switch (seismicIntensity) {
+                case 10:
+                    color = 'red';
+                    break;
+                case 20:
+                    color = 'orange';
+                    break;
+                case 30:
+                    color = 'yellow';
+                    break;
+                // Add more cases as needed for other seismic intensities
+                default:
+                    color = 'gray';
+            }
+
+            // Find the corresponding prefecture in the geojson data and update its style
+            $.each(data.features, function(index, feature) {
+                if (feature.properties.pref === prefectureCode) {
+                    feature.properties['fillColor'] = color;
+                    feature.properties['fillOpacity'] = 0.5; // Adjust opacity as needed
+                    return false; // Exit loop once the prefecture is found
+                }
+            });
+        });
+
+        // Update the map with the modified geojson data
+        L.geoJson(data, {
+            style: function(feature) {
+                return {
+                    fillColor: feature.properties.fillColor,
+                    fillOpacity: feature.properties.fillOpacity,
+                    color: '#ffffff',
+                    weight: 1.5,
+                    opacity: 1
+                };
+            }
+        }).addTo(map);
+    });
 });
