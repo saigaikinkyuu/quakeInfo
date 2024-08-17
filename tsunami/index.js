b55d9a64d8cdaef62aa173f97a58806ab6f8d634
saigaikinkyuu <125773463+saigaikinkyuu@users.noreply.github.com>
2024-08-17 14:33:33 +0900

    Update index.js

--- tsunami/index.js
+++ tsunami/index.js
@@ -136,13 +136,13 @@ function drawMap(){
 		  color: "black",
 		  fillColor: color,
 		  fillOpacity: 1,
-		  zIndexOffset: 4000,
+		  zIndexOffset: 20000,
 		  pane: 'markerPane'
 		}).addTo(map);
 		// 地図にマーカーを追加
 		marker.bindPopup("<div style='text-align: center;'>" + observation_items[n].Area.Name + stations[s].Name + "<br>第一波の状況：" + stations[s].FirstHeight.Condition + "<br>最大波(観測)：" + stations[s].MaxHeight.TsunamiHeight + "m (" + new Date(stations[s].MaxHeight.DateTime).getDate() + "日 " + new Date(stations[s].MaxHeight.DateTime).getHours() + "時" + new Date(stations[s].MaxHeight.DateTime).getMinutes() + "分)</div>", {
 		  closeButton: false,
-		  zIndexOffset: 4000,
+		  zIndexOffset: 20000,
 		  maxWidth: 10000
 		});
               }
