466481a35a3a0f7ccb61e857023ff9836c7f36c9
saigaikinkyuu <125773463+saigaikinkyuu@users.noreply.github.com>
2024-08-17 14:32:52 +0900

    Update index.js

--- tsunami/index.js
+++ tsunami/index.js
@@ -54,7 +54,7 @@ function drawMap(){
 	  markerPane.style.zIndex = 10000; // Zインデックスを設定
 	  // 新しいペインを作成
 	  var linePane = map.createPane('linePane');
-	  linePane.style.zIndex = 4000; // Zインデックスを設定
+	  linePane.style.zIndex = 2000; // Zインデックスを設定
           forecast_items = data.Body.Tsunami.Forecast.Item
           var areaNameArray = [ '北海道太平洋沿岸東部','北海道太平洋沿岸中部','北海道太平洋沿岸西部','北海道日本海沿岸北部','北海道日本海沿岸南部','オホーツク海沿岸','青森県日本海沿岸','青森県太平洋沿岸','陸奥湾','岩手県','宮城県','秋田県','山形県','福島県','茨城県','千葉県九十九里・外房','千葉県内房','東京湾内湾','伊豆諸島','小笠原諸島','相模湾・三浦半島','新潟県上中下越','佐渡','富山県','石川県能登','石川県加賀','福井県','静岡県','愛知県外海','伊勢・三河湾','三重県南部','京都府','大阪府','兵庫県北部','兵庫県瀬戸内海沿岸','淡路島南部','和歌山県','鳥取県','島根県出雲・石見','隠岐','岡山県','広島県','徳島県','香川県','愛媛県宇和海沿岸','愛媛県瀬戸内海沿岸','高知県','山口県日本海沿岸','山口県瀬戸内海沿岸','福岡県瀬戸内海沿岸','福岡県日本海沿岸','有明・八代海','佐賀県北部','長崎県西方','壱岐・対馬','熊本県天草灘沿岸','大分県瀬戸内海沿岸','大分県豊後水道沿岸','宮崎県','鹿児島県東部','種子島・屋久島地方','奄美群島・トカラ列島','鹿児島県西部','沖縄本島地方','大東島地方','宮古島・八重山地方' ]
           var areaNumArray = [ '100','101','102','110','111','120','200','201','202','210','220','230','240','250','300','310','311','312','320','321','330','340','341','350','360','361','370','380','390','391','400','500','510','520','521','522','530','540','550','551','560','570','580','590','600','601','610','700','701','710','711','712','720','730','731','740','750','751','760','770','771','772','773','800','801','802' ]
@@ -136,13 +136,13 @@ function drawMap(){
 		  color: "black",
 		  fillColor: color,
 		  fillOpacity: 1,
-		  zIndexOffset: 2000,
+		  zIndexOffset: 4000,
 		  pane: 'markerPane'
 		}).addTo(map);
 		// 地図にマーカーを追加
 		marker.bindPopup("<div style='text-align: center;'>" + observation_items[n].Area.Name + stations[s].Name + "<br>第一波の状況：" + stations[s].FirstHeight.Condition + "<br>最大波(観測)：" + stations[s].MaxHeight.TsunamiHeight + "m (" + new Date(stations[s].MaxHeight.DateTime).getDate() + "日 " + new Date(stations[s].MaxHeight.DateTime).getHours() + "時" + new Date(stations[s].MaxHeight.DateTime).getMinutes() + "分)</div>", {
 		  closeButton: false,
-		  zIndexOffset: 2000,
+		  zIndexOffset: 4000,
 		  maxWidth: 10000
 		});
               }
