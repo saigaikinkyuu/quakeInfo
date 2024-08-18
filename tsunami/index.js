dc2f56f2c97b81013a8b0c9d9e913071abb03b0c
saigaikinkyuu <125773463+saigaikinkyuu@users.noreply.github.com>
2024-08-18 10:46:13 +0900

    Update index.js

--- tsunami/index.js
+++ tsunami/index.js
@@ -190,32 +190,3 @@ function drawMap(){
     })
 }
 drawMap()
-/*window.onload = function(){
-  document.getElementById("container").style.display = "block"
-  $('#reading').fadeOut(700);
-}*/
-(function($) {
-
- /** インジケータ表示範囲を管理するコントローラ */
- var indicatorController = {
-
-  __name: 'indicatorController',
-
-  /** インジケータ表示ボタン押下イベント */
-  '#indicator click': function(){
-
-   //インジケータ表示
-   var indicator = this.indicator({
-    message: 'block'
-   }).show();
-
-   setTimeout(function() {
-
-    //インジケータ除去
-    indicator.hide();
-
-   }, 800);
-  }
- };
- $(function(){ h5.core.controller('#container', indicatorController)});
-})(jQuery);
